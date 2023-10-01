b3203af639c283bc05908f7c36046539fc0c9fcf
Castro <Castro@DESKTOP-TM0V60U>
2023-10-01 14:28:38 +0800

    revised

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,78 +1,114 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package calculator;
+
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-/**
- *
- * @author User
- */
-public class Calculator {
+import java.awt.event.ActionListener;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        Frame frame = new Frame("Simple Calculator");
-        frame.setSize(500, 500);
-        frame.setLayout(null);
-        frame.setName("Simple Calculator");
-        
-        TextField text1 = new TextField();
-        text1.setName("num1TextField");
-        text1.setBounds(100, 50, 130, 30);
-        
-        TextField text2 = new TextField();
-        text2.setName("num2TextField");
-        text2.setBounds(300, 50, 130, 30);
-        
-        Choice c = new Choice();
-        c.setBounds(100,90,100,40);
-        c.setName("operationsChoice");
-        c.add("+");
-        c.add("-");
-        c.add("*");
-        c.add("/");
-        
-        Button b = new Button("Compute Result");
-        b.setName("computeButton");
-        b.setBounds(100,150,90,30);
-        
-        Label l = new Label("");
-        l.setName("resultLabel");
-        l.setBounds(100,200,200,30);
-        
-        b.addActionListener((ActionEvent e)->{
-            int num1 = Integer.parseInt(text1.getText());
-            int num2 = Integer.parseInt(text2.getText());
-            String operation = c.getSelectedItem();
-            int result = 0;
-            
-            switch(operation){
-                case "+":
-                result = num1 + num2;
-                break;
-                case "-":
-                result = num1 - num2;
-                break;
-                case "*":
-                result = num1 * num2;
-                break;
-                case "/":
-                result = num1 / num2;
-                break;
-            }
-            l.setText(String.valueOf(result));
-        });
-        
-        frame.add(l);
-        frame.add(b);
-        frame.add(c);
-        frame.add(text1);
-        frame.add(text2);
-        frame.setVisible(true);
+public class Calculator extends JFrame implements ActionListener {
+    // Components
+    private JTextField display;
+    private JButton[] numberButtons;
+    private JButton[] operationButtons;
+    private JButton equalsButton;
+    private JButton clearButton;
+
+    // Variables
+    private double firstNumber = 0;
+    private String operator = "";
+    private boolean isOperatorClicked = false;
+
+    public Calculator() {
+        setTitle("Simple Calculator");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 400);
+        setLayout(new GridLayout(5,4 ));
+
+        display = new JTextField();
+        display.setEditable(false);
+        add(display);
+
+        numberButtons = new JButton[10];
+        for (int i = 0; i < 10; i++) {
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            add(numberButtons[i]);
+        }
+
+        operationButtons = new JButton[4];
+        operationButtons[0] = new JButton("+");
+        operationButtons[1] = new JButton("-");
+        operationButtons[2] = new JButton("*");
+        operationButtons[3] = new JButton("/");
+        for (int i = 0; i < 4; i++) {
+            operationButtons[i].addActionListener(this);
+            add(operationButtons[i]);
+        }
+
+        equalsButton = new JButton("=");
+        equalsButton.addActionListener(this);
+        add(equalsButton);
+
+        clearButton = new JButton("C");
+        clearButton.addActionListener(this);
+        add(clearButton);
+
+        setVisible(true);
     }
-    
+
+    public static void main(String[] args) {
+        new Calculator();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String command = e.getActionCommand();
+        if (Character.isDigit(command.charAt(0))) {
+            if (isOperatorClicked) {
+                display.setText(command);
+                isOperatorClicked = false;
+            } else {
+                display.setText(display.getText() + command);
+            }
+        } else if (command.equals("+") || command.equals("-") || command.equals("*") || command.equals("/")) {
+            if (!operator.isEmpty()) {
+                calculate();
+            }
+            firstNumber = Double.parseDouble(display.getText());
+            operator = command;
+            isOperatorClicked = true;
+        } else if (command.equals("=")) {
+            calculate();
+        } else if (command.equals("C")) {
+            display.setText("");
+            firstNumber = 0;
+            operator = "";
+        }
+    }
+
+    private void calculate() {
+        double secondNumber = Double.parseDouble(display.getText());
+        double result = 0;
+        switch (operator) {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            case "*":
+                result = firstNumber * secondNumber;
+                break;
+            case "/":
+                if (secondNumber != 0) {
+                    result = firstNumber / secondNumber;
+                } else {
+                    display.setText("Error");
+                    return;
+                }
+                break;
+        }
+        display.setText(String.valueOf(result));
+        operator = "";
+    }
 }
